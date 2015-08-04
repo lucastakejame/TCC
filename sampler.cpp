42dd0297a6e6807a1f5e514f8f40856cbd67e246
Lucas <lucas@takejame.com.br>
2015-08-04 01:10:01 -0300

    Eliminated class, refactored code and added src column profile

--- sampler.cpp
+++ sampler.cpp
@@ -9,35 +9,26 @@
 using namespace std;
 using namespace cv;
 
-const int limit_scal = 99;
-const int limit_sz_kernel = 20;
-const int limit_sel_col = 9999;
+const int LIMIT_ALPHA = 9;
+const int LIMIT_SZ_KERNEL = 20;
+const int LIMIT_SEL_COL = 9999;
+const int TRACE_WIDTH = 12;
+
+static int g_prm_alpha = 2;
+static int g_prm_sz_kernel = 5;
+static int g_prm_sel_col = 4040;
 
 
-// void resize_imshow(const char* window_name, const Mat& src)
-// {
-//     const int max_size = 500;
+Mat g_mat_src;
+Mat g_mat_dy;
+
+static void cb_filter(int param, void* user_data);
+static void cb_col_profile(int param, void* user_data);
+static void col_profile(const Mat& src, int src_col, const char* window_name);
+static void paint_col(Mat src, int src_col);
 
 
-//     if(src.cols > max_size || src.rows > max_size)
-//     {
-//         Mat src_resize;
-
-//         float sz_new_width = max_size;
-//         float sz_new_height = sz_new_width*src.rows/src.cols;
-
-//         Size size(sz_new_width, sz_new_height);
-
-//         resize(src, src_resize, size);
-
-//         imshow(window_name, src_resize);
-//     }
-//     else
-//     {
-//         imshow(window_name, src);
-//     }
-// }
-
+// resize matrix to show
 Mat resize_mat(const Mat& src)
 {
     const int max_size = 500;
@@ -61,122 +52,110 @@ Mat resize_mat(const Mat& src)
     return src_resize;
 }
 
-// Filter Parameters
-class FParams
+// This callback purpuse is to get the d/dy derivative matrix from source matrix
+static void cb_filter(int param, void* user_data)
 {
-public:
-    static int prm_sel_col; // prm_: parameter used in GUI
-    static int prm_sz_kernel;
-    static int prm_scal;
+    float alpha;
+    if(g_prm_alpha < 1)
+        alpha = 0.1;
+    else
+        alpha = (float)g_prm_alpha/10;
 
-    Mat* pt_mat_src;
-    Mat* pt_mat_dst;
-    Mat  mat_kernel;
+    int sz_kernel = alpha*TRACE_WIDTH;
 
-    FParams(Mat* src, Mat* dst):pt_mat_src(src), pt_mat_dst(dst)
+    if(sz_kernel%2 == 0)
+        sz_kernel++;
+
+    Mat mat_kernel = Mat::ones( sz_kernel, 1, CV_32F );
+    for (int i = 0; i < sz_kernel/2; ++i)
     {
+        mat_kernel.at<float>(i, 0) = -1;
     }
+    mat_kernel.at<float>(sz_kernel/2, 0) = 0;
 
-    static void cb_filter(int param, void* user_data)
-    {
-        FParams* p_hndl = (FParams *)user_data; // pointer handle
+    // Apply filter
+    Point arg_anchor = Point( -1, -1 );
+    double arg_delta = 0;
+    int arg_ddepth = -1;
+    filter2D(g_mat_src, g_mat_dy, arg_ddepth , mat_kernel, arg_anchor, arg_delta, BORDER_DEFAULT );
 
-        Mat show_dst = resize_mat(*(p_hndl->pt_mat_dst));
+    cb_col_profile(0,0);
+}
 
-        p_hndl->mat_kernel = Mat::zeros( FParams::prm_sz_kernel, FParams::prm_sz_kernel, CV_32F );
-        for (int i = 0; i < FParams::prm_sz_kernel/2; ++i)
-        {
-            p_hndl->mat_kernel.at<float>(i, FParams::prm_sz_kernel/2) = -((float)FParams::prm_scal/10);
-            p_hndl->mat_kernel.at<float>( (FParams::prm_sz_kernel-1) - i, FParams::prm_sz_kernel/2) = ((float)FParams::prm_scal/10);
-        }
+static void cb_col_profile(int param, void* user_data)
+{
+    Mat show_mat_dy = resize_mat(g_mat_dy);
 
-        // Apply filter
+    // normalized sel_col param, bounded in [0,1]
+    float norm_sel_col = (float)g_prm_sel_col/LIMIT_SEL_COL;
 
-        if(FParams::prm_sz_kernel)
-        {
-            Point arg_anchor = Point( -1, -1 );
-            double arg_delta = 0;
-            int arg_ddepth = -1;
+    int orig_mat_col = (float)(g_mat_dy.cols-1)*norm_sel_col;
 
-            filter2D(*(p_hndl->pt_mat_src), *(p_hndl->pt_mat_dst), arg_ddepth , p_hndl->mat_kernel, arg_anchor, arg_delta, BORDER_DEFAULT );
-        }
+    col_profile(g_mat_dy, orig_mat_col, "Perfil");
+    col_profile(g_mat_src, orig_mat_col, "Perfil src");
 
-        cb_col_contour(0,p_hndl);
+    int show_mat_col;
+    if(show_mat_col > orig_mat_col)
+        show_mat_col = orig_mat_col*(float)(show_mat_dy.cols)/(g_mat_dy.cols);
+    else
+        show_mat_col = (float)(show_mat_dy.cols-1)*norm_sel_col;
 
-    }
+    paint_col(show_mat_dy, show_mat_col);
 
-    static void cb_col_contour(int param, void* user_data)
-    {
-        FParams* p_hndl = (FParams *)user_data;
+    imshow( "filter2D Demo", show_mat_dy );
 
-        Mat show_mat = resize_mat(*(p_hndl->pt_mat_dst));
+}
 
-        float norm_sel_col = (float)FParams::prm_sel_col/limit_sel_col; // normalized sel_col param. []
-        int orig_mat_col = (float)(p_hndl->pt_mat_dst->cols-1)*norm_sel_col;
-        int show_mat_col;
-        if(show_mat_col > orig_mat_col)
-            show_mat_col = orig_mat_col*(float)(show_mat.cols)/(p_hndl->pt_mat_dst->cols);
-        else
-            show_mat_col = (float)(show_mat.cols-1)*norm_sel_col;
+static void col_profile(const Mat& mat_src, int col_src, const char* window_name)
+{
+    unsigned char *input = (unsigned char*)(mat_src.data);
+    input = input + col_src;
+    CvPlot::clear(window_name);
+    CvPlot::plot(window_name, input, mat_src.rows, mat_src.cols, 253, 150, 114);
+    CvPlot::label("Column Profile");
+}
 
-        unsigned char *input = (unsigned char*)(p_hndl->pt_mat_dst->data);
-        input = input + orig_mat_col;
-        CvPlot::clear("Perfil");
-        CvPlot::plot("Perfil", input, p_hndl->pt_mat_dst->rows, p_hndl->pt_mat_dst->cols, 253, 150, 114);
-        CvPlot::label("Column Section");
-
-        Point start = Point(show_mat_col,0);
-        Point end = Point(show_mat_col, show_mat.rows-1);
-        int thickness = 1;
-        int line_type = 8;
-        line( show_mat, start, end, Scalar(255), thickness, line_type );
-
-        imshow( "filter2D Demo", show_mat );
-
-    }
-};
-
-
-int FParams::prm_scal = 10;
-int FParams::prm_sz_kernel = 5;
-int FParams::prm_sel_col = 4040;
-
+static void paint_col(Mat mat_src, int col_src)
+{
+    Point start = Point(col_src,0);
+    Point end = Point(col_src, mat_src.rows-1);
+    int thickness = 1;
+    int line_type = 8;
+    line(mat_src, start, end, Scalar(255), thickness, line_type);
+}
 
 /** @function main */
 int main ( int argc, char** argv )
 {
-/// Declare variables
-    Mat mat_src, mat_dy; // mat_: matrix
+    // Load an image
+    g_mat_src = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
-/// Load an image
-    mat_src = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-
-    if(mat_src.rows > mat_src.cols)
+    // This force the image to be in a landscape format
+    if(g_mat_src.rows > g_mat_src.cols)
     {
-        Mat temp = mat_src.t();
-        mat_src = temp;
+        g_mat_src = g_mat_src.t();
     }
 
-    mat_dy.create(mat_src.rows, mat_src.cols, CV_8UC1);
+    // negative src
+    bitwise_not(g_mat_src, g_mat_src);
 
-    FParams hndl_filter(&mat_src, &mat_dy); // handle for filter parameters
+    // g_mat_dy.create(g_mat_src.rows, g_mat_src.cols, CV_32FC1);
+    g_mat_dy.create(g_mat_src.rows, g_mat_src.cols, CV_8UC1);
 
-    if( !mat_src.data )
+    if( !g_mat_src.data )
         { return -1; }
 
-/// Create window
+    // Create window
     namedWindow( "filter2D Demo", CV_WINDOW_AUTOSIZE );
     namedWindow( "original", CV_WINDOW_AUTOSIZE );
 
-/// Create trackbars
-    createTrackbar( "kernel scalar", "original", &FParams::prm_scal, limit_scal, FParams::cb_filter, &hndl_filter);
-    createTrackbar( "kernel size", "original", &FParams::prm_sz_kernel, limit_sz_kernel, FParams::cb_filter, &hndl_filter);
-    createTrackbar( "Column section", "original", &FParams::prm_sel_col, limit_sel_col, FParams::cb_col_contour, &hndl_filter);
+    // Create trackbars
+    createTrackbar( "kernel alpha", "original", &g_prm_alpha, LIMIT_ALPHA, cb_filter);
+    createTrackbar( "Column section", "original", &g_prm_sel_col, LIMIT_SEL_COL, cb_col_profile);
 
-
-/// open refreshed windows
-    FParams::cb_filter(0, &hndl_filter);
-    imshow( "original", resize_mat(mat_src) );
+    // open refreshed windows
+    cb_filter(0, 0);
+    imshow( "original", resize_mat(g_mat_src) );
 
     waitKey(0);
 
